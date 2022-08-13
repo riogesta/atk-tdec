e9e9ec18e1900a9423df189707d21fa8b6a9514f
riogesta <riogesta.v@gmail.com>
2022-08-13 10:30:53 +0700

    update

--- app/Views/index.php
+++ app/Views/index.php
@@ -1,137 +1,99 @@
-  <!-- Content -->
+<!-- Content -->
 
-  <div class="container-xxl flex-grow-1 container-p-y">
-    <div class="row">
+<div class="container-xxl flex-grow-1 container-p-y">
+   <div class="row mb-2">
       <!-- selamat datang -->
-      <div class="col-lg-8 mb-4 order-0">
-        <div class="card">
-          <div class="d-flex align-items-end row">
-            <div class="col-sm-7">
-              <div class="card-body">
-                <h5 class="card-title text-primary">Selamat Datang </h5>
-                <p class="mb-4">
-                  Kasubag Umum
-                </p>
 
-              </div>
-            </div>
-            <div class="col-sm-5 text-center text-sm-left">
-              <div class="card-body pb-0 px-0 px-md-4">
-                <img src="../assets/img/illustrations/man-with-laptop-light.png" height="140" alt="View Badge User"
-                  data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                  data-app-light-img="illustrations/man-with-laptop-light.png" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- total data barang -->
-      <div class="col-4">
-        <div class="row g-0">
-          <div class="mb-4">
-            <div class="card">
-              <div class="card-body">
-                <div class="d-flex justify-content-between flex-sm-row flex-column mb-3">
-                  <div class="d-flex align-items-end row">
-                    <div class="align-items-center">
-                      <h6 class="card-title text-primary mb-2">Total Data Barang</h6>
-                      <p class="mb-3">$84,686k</p>
-                    </div>
-                  </div>
-                  <div class="col-lg-5 text-center text-sm-left">
-                    <img class="card-body pb-2 px-2 px-md-3" src="../assets/img/icons/unicons/archive-filled-box.png"
-                      alt="Card image" width="100">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!-- Total pengajuan -->
-      <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
-        <div class="card">
-          <div class="row row-bordered g-0">
-            <div class="col-md-8">
-              <h5 class="card-header m-0 me-2 pb-3">Total Pengajuan</h5>
-              <div id="totalRevenueChart" class="px-2"></div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <!--/ Total stok barang -->
-      <div class="col-4 order-md-2">
-        <div class="row g-0">
-          <div class="mb-4">
-            <div class="card">
-              <div class="card-body">
-                <div class="d-flex justify-content-between flex-sm-row flex-column mb-3">
-                  <div class="d-flex align-items-end row">
-                    <div class="align-items-center">
-                      <h6 class="card-title text-primary mb-2">Total Data Barang</h6>
-                      <p class="mb-3">$84,686k</p>
-                    </div>
-                  </div>
-                  <div class="col-lg-5 text-center text-sm-left">
-                    <img class="card-body pb-2 px-2 px-md-3" src="../assets/img/icons/unicons/aa.png" alt="Card image"
-                      width="100">
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- <div class="col-6 mb-4">
-                    <div class="card">
-                      <div class="card-body">
-                        <div class="card-title d-flex align-items-start justify-content-between">
-                          <div class="avatar flex-shrink-0">
-                            <img src="../assets/img/icons/unicons/cc-primary.png" alt="Credit Card" class="rounded" />
-                          </div>
-                          <div class="dropdown">
-                            <button class="btn p-0" type="button" id="cardOpt1" data-bs-toggle="dropdown"
-                              aria-haspopup="true" aria-expanded="false">
-                              <i class="bx bx-dots-vertical-rounded"></i>
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="cardOpt1">
-                              <a class="dropdown-item" href="javascript:void(0);">View More</a>
-                              <a class="dropdown-item" href="javascript:void(0);">Delete</a>
-                            </div>
-                          </div>
-                        </div>
-                        <span class="fw-semibold d-block mb-1">Transactions</span>
-                        <h3 class="card-title mb-2">$14,857</h3>
-                        <small class="text-success fw-semibold"><i class="bx bx-up-arrow-alt"></i> +28.14%</small>
-                      </div>
-                    </div>
-                  </div> -->
-      <!-- </div>
-    <div class="row"> -->
-      <!-- <div class="col-12 mb-4">
-                <div class="card">
+      <!-- total data user -->
+      <div class="col-lg-4">
+         <div class="card mb-3">
+            <div class="row g-0">
+               <div class="col-md-8">
                   <div class="card-body">
-                    <div class="d-flex justify-content-between flex-sm-row flex-column gap-3">
-                      <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
-                        <div class="card-title">
-                          <h5 class="text-nowrap mb-2">Profile Report</h5>
-                          <span class="badge bg-label-warning rounded-pill">Year 2021</span>
-                        </div>
-                        <div class="mt-sm-auto">
-                          <small class="text-success text-nowrap fw-semibold"><i class="bx bx-chevron-up"></i>
-                            68.2%</small>
-                          <h3 class="mb-0">$84,686k</h3>
-                        </div>
-                      </div>
-                      <div id="profileReportChart"></div>
-                    </div>
+                     <h5 class="card-title text-primary">Jumlah User</h5>
+                     <p class="card-text">$84.111k<p class="card-text">
                   </div>
-                </div>
-              </div> -->
-      <!-- </div> -->
-    </div>
-  </div>
-  </div>
+               </div>
+               <div class="col-md-4 px-2 py-2">
+                  <img class="card-img card-img-right-center" src="../assets/img/icons/unicons/man.png"
+                     alt="Card image" />
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <!-- total permintaan -->
+      <div class="col-lg-4">
+         <div class="card mb-3">
+            <div class="row g-0">
+               <div class="col-md-8">
+                  <div class="card-body">
+                     <h5 class="card-title text-primary">Jumlah Pengajuan</h5>
+                     <p class="card-text">$84.111k<p class="card-text">
+                  </div>
+               </div>
+               <div class="col-md-4 px-2 py-2">
+                  <img class="card-img card-img-right" src="../assets/img/icons/unicons/checklist.png"
+                     alt="Card image" />
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <!--/ Total data barang -->
+      <div class="col-lg-4">
+         <div class="card mb-3">
+            <div class="row g-0">
+               <div class="col-md-8">
+                  <div class="card-body">
+                     <h5 class="card-title text-primary">Jumlah Barang</h5>
+                     <p class="card-text">$84.111k<p class="card-text">
+                  </div>
+               </div>
+               <div class="col-md-4 px-2 py-2">
+                  <img class="card-img card-img-right" src="../assets/img/icons/unicons/box.png" alt="Card image" />
+               </div>
+            </div>
+         </div>
+      </div>
+      <!-- row -->
+   </div>
+
+   <!-- Total pengajuan -->
+   <div class="col-xxl">
+      <div class="card mb-4">
+         <div class="card-header d-flex align-items justify-content-between">
+            <h5 class="mb-0">Daftar Pengajuan</h5>
+            <!-- <small class="text-muted ">Default label</small> -->
+         </div>
+         <div class="table-responsive text-nowrap">
+            <table class="table table-sm">
+               <thead>
+                  <tr>
+                     <th class="px-4">No</th>
+                     <th class="text-center">Nama Barang</th>
+                     <th class="text-center">Satuan</th>
+                     <th class="text-center">Tanggal Pengajuan</th>
+                     <th class="text-center">Unit / Prodi</th>
+                  </tr>
+               </thead>
+               <tbody class="table-border-bottom-0">
+                  <tr>
+                     <td><i class="fab fa-angular fa-lg text-danger me-3"></i> <strong>1</strong>
+                     </td>
+                     <td class="text-center">Amplop Coklat Polos</td>
+                     <td class="text-center">Lembar</td>
+                     <td class="text-center">04 Agustus 2022</td>
+                     <td class="text-center">Akuntansi</td>
+                  </tr>
+               </tbody>
+            </table>
+         </div>
+      </div>
+      <!--/ Small table -->
+      <!-- Content wrapper -->
+   </div>
+
+
+</div>
+</div>
