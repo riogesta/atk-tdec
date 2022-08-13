650f82e74584867f123e58078f584bac196259cd
riogesta <riogesta.v@gmail.com>
2022-08-13 10:13:31 +0700

    update

--- app/Views/layout/nav.php
+++ app/Views/layout/nav.php
@@ -24,50 +24,7 @@
 			<ul class="navbar-nav flex-row align-items-center ms-auto">
 
 				<!-- User -->
-				<li class="nav-item navbar-dropdown dropdown-user dropdown">
-					<a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
-						<div class="avatar avatar-online">
-							<img src="../assets/img/avatars/1.png" alt class="w-px-40 h-auto rounded-circle" />
-						</div>
-					</a>
-					<ul class="dropdown-menu dropdown-menu-end">
-						<li>
-							<a class="dropdown-item" href="#">
-								<div class="d-flex">
-									<div class="flex-shrink-0 me-3">
-										<div class="avatar avatar-online">
-											<img src="../assets/img/avatars/1.png" alt="profile picture"
-												class="w-px-40 h-auto rounded-circle" />
-										</div>
-									</div>
-									<div class="flex-grow-1">
-										<span class="fw-semibold d-block"><?= esc($_SESSION['UNIT-PRODI']) ?></span>
-										<small class="text-muted"><?= esc($_SESSION['USER']) ?></small>
-									</div>
-								</div>
-							</a>
-						</li>
 
-
-						<li>
-							<div class="dropdown-divider"></div>
-						</li>
-						<?php if($_SESSION['ROLE'] == '0') { ?>
-						<li>
-							<a class="dropdown-item" href="/logout">
-								<i class='bx bx-cog'></i>
-								<span class="align-middle">Pengaturan Akun</span>
-							</a>
-						</li>
-						<?php } ?>
-						<li>
-							<a class="dropdown-item" href="/logout">
-								<i class="bx bx-power-off me-2"></i>
-								<span class="align-middle">Log Out</span>
-							</a>
-						</li>
-					</ul>
-				</li>
 				<!--/ User -->
 			</ul>
 		</div>
