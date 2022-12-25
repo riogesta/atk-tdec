e0db706b30eb6cd8cb88d5d444b0f01368cdd970
riogesta <riogesta.v@gmail.com>
2022-12-25 09:59:01 +0700

    update

--- app/Views/layout/head.php
+++ app/Views/layout/head.php
@@ -15,10 +15,11 @@
     <link rel="icon" type="image/x-icon" href="<?= base_url('/assets/img/favicon/favicon.ico') ?>" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
 
+    <!-- font -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&family=Rubik:ital,wght@0,400;0,700;1,400;1,700&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Inter:wght@500;750&family=Roboto:ital,wght@0,400;0,700;1,400&display=swap"
         rel="stylesheet">
 
     <!-- jQuery -->
@@ -104,7 +105,7 @@
         }
 
         body {
-            font-family: 'Rubik', 'sans-serif';
+            font-family: 'Inter', 'sans-serif';
         }
     </style>
 </head>
